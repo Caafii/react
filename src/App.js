93156850670e9b505c0e8b36b6b06991acb0a4bb
Caafii <157214556+Caafii@users.noreply.github.com>
2025-03-16 12:28:04 +0300

    list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const nameClicker = () => {
+    console.log(students);
+  };
+  const students = ["cali", "farax", "cabdi", " cumar"];
+  return students.map((student, index) => (
+    <>
+      
+      <li onClick={nameClicker} key={index}>
+      
+        {student}
+      </li>
+
+      <input type="text" placeholder="Enter name"></input>
+    </>
+  ),
+ 
+ 
+);
 }
 
 export default App;
